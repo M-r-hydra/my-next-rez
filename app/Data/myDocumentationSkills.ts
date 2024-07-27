188989b371212f82230cf28c87597a67241267b9
Ali Goodarzi <me74242@gmail.com>
2024-07-27 16:50:55 +0330

    #

--- app/Data/myDocumentationSkills.ts
+++ app/Data/myDocumentationSkills.ts
@@ -1,6 +1,11 @@
 export const myDocumentationSkills = [
   {
     id: "document 1",
+    name: "PostMan ",
+    value: 80,
+  },
+  {
+    id: "document 1",
     name: "Swagger ",
     value: 70,
   },
